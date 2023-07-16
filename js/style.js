beab993a7abb68caee7d9fe82998560398478d69
roadman-KT3 <nanakweku1997@gmail.com>
2023-07-16 19:34:31 +0000

    finished building the game, adding ui tomorrow

--- js/style.js
+++ js/style.js
@@ -15,9 +15,9 @@ if (typeof document !== 'undefined') {
     let computerScore = 0;
     let player = document.querySelector("#playerScore");
     let computer = document.querySelector("#computerScore");
-    let noOfPlay = 5;
+    let reset = document.getElementById("reset-btn");
 
-
+    
     const buttons = document.querySelectorAll(".btn");
     buttons.forEach((button) =>{button.addEventListener('click', () =>
     {
@@ -44,55 +44,38 @@ if (typeof document !== 'undefined') {
 
         
 function playRound(playerSelection, computerSelection) {
-    
-    if (playerSelection === computerSelection) {
-        results.textContent = `It's a tie! you choose ${playerSelection} and computer choose ${computerSelection}`;
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-        ) {
-            playerScore ++;
-            player.textContent = `Player Score: ${playerScore}`
-            computer.textContent = `Computer Score: ${computerScore}`
-            results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-        } 
-        else {
-            computerScore ++;
-            player.textContent = `Player Score: ${playerScore}`
-            computer.textContent = `Computer Score: ${computerScore}`
-            results.textContent =`You Lose! ${computerSelection} beats ${playerSelection}`;
+    if ((playerScore || computerScore) < 5){
+        if (playerSelection === computerSelection) {
+            results.textContent = `It's a tie! you choose ${playerSelection} and computer choose ${computerSelection}`;
+        } else if (
+            (playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'paper' && computerSelection === 'rock') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper')
+            ) {
+                    
+                    playerScore ++;
+                    player.textContent = `Player Score: ${playerScore}`
+                    computer.textContent = `Computer Score: ${computerScore}`
+                    results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+            } 
+            else {
+                computerScore ++;
+                player.textContent = `Player Score: ${playerScore}`
+                computer.textContent = `Computer Score: ${computerScore}`
+                results.textContent =`You Lose! ${computerSelection} beats ${playerSelection}`;
+            }
         }
-    }
-    // if (playerScore === 5) {
-    //     results.textContent = 'Player wins!';
-    // } else if (computerScore === 5) {
-    //     results.textContent = 'Computer wins!';
-    // }
-
+        else{
+            results.textContent = `Game Over`;
+        }
+    };
     
-     
+    reset.addEventListener('click', () =>
+    {
+        player.textContent = `Player Score: ${playerScore = 0}`
+        computer.textContent = `Computer Score: ${computerScore = 0}`
+                
+    }) 
     
-    }
-    
-    
-
-// // // Display the results of the game
-// // const resultsDiv = document.querySelector('.results');
-
-// // function displayResults(message) {
-// //   message = game();
-// //   const p = document.createElement('p');
-// //   p.textContent = message;
-// //   resultsDiv.appendChild(p);
-// // }
-
-// // // Replace all console.log statements with displayResults, for example:
-
-// // displayResults(`Player: ${playerSelection} | Computer: ${computerSelection}`);
-
-// //  a NEW function called game(). 
-// // Use the previous function inside 
-// // of this one to play a 5 round game that keeps score
-// // and reports a winner or loser at the end.
+};
 
